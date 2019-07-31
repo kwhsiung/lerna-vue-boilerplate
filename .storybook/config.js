8f65d5d03c85829cde48d9b7e4730332ecd97731
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-07-31 17:47:03 +0800

    chore: init

---
+++ .storybook/config.js
@@ -0,0 +1,7 @@
+import { configure } from '@storybook/vue'
+// automatically import all files ending in *.stories.js
+const req = require.context('../packages', true, /\.stories\.js$/)
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+configure(loadStories, module)
